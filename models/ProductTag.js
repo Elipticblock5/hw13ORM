823af8b39a831687e66c1e3e82942778a6e14398
EliptickBlock5 <nbjohnson@gmail.com>
2021-05-02 12:00:10 -0700

    adding tag association methods

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -13,7 +13,7 @@ ProductTag.init(
       primaryKey: true,
       autoIncrement: true
     },
-
+  //added product and tag ids
     product_id: {
       type: DataTypes.INTEGER,
       references: {
@@ -28,7 +28,7 @@ ProductTag.init(
         key: 'id'
       }
     }
-    
+
   },
   {
     sequelize,
